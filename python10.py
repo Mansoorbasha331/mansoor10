c3a77e928d9492f6709214adff26d7415ddc7f8b
Mansoorbasha331 <162982681+Mansoorbasha331@users.noreply.github.com>
2024-03-21 11:37:05 +0530

    Add files via upload

---
+++ python10.py
@@ -0,0 +1,44 @@
+polymorphism in classes
+we can achieve polymorphism in 2 ways
+1.) method overloading --> it is not possible in python
+2.) method overriding
+
+tasks
+write the code for the below tasks using function
+1.)d1 = {"shirt": 1000, "pant":1500, "shoes": "900", "handkey": 30}
+a.) find the min ans max priced product
+b.) find the product starts with 's' and 's'
+
+2.) find the n = 67, is strong number or not
+
+3.) l1 = [1,2,3,4,5,6]
+n=2 --> [5, 6, 1,2,3,4]
+n=3 --> [4,5,6, 1,2,3]
+# method riding
+#*polymyresation in classes
+class bank:
+    def ratio( self )
+    print ("all banks has repo rate ")
+#class SBI (bank):
+ #   def ratio (self )
+#print ("sBI rate is 9%)
+#class ibo (bank ):
+#def ratio (self )
+ #print ("iob rate is 7.5%")
+
+#i=IOB()
+ #.ratio()
+ #s= SBI()
+ #.ratio()
+ #?Eg:2
+ 
+class India(USA):
+    def language(self):
+        print("None")
+
+    def capital(self):
+         print("New delhi")
+
+#I = India()
+#I.language()
+#I.capital()
